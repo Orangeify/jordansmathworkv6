c47732d40ba5dfd477d0aee4ceffcf206089e6fb
Orangeify <187468059+Orangeify@users.noreply.github.com>
2024-11-23 20:39:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/jmwsw/',
+    bare: 'https://parallel-daphene-ijuslostmydooohg-021a963b.koyeb.app/fq/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
